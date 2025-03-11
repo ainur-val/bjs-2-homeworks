3f0f9c79a58c1554c05f0b887673f7f4467cd94b
Aynur Valiev <ainur-val@mail.ru>
2025-03-11 22:19:53 +0500

    Внес изменения в 6 задание

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -19,21 +19,22 @@ class Triangle {
 		this.side1 = side1;
 		this.side2 = side2;
 		this.side3 = side3;
+		
+			if (this.side1 + this.side2 < this.side3 || this.side1 + this.side3 < this.side2 || this.side2 + this.side3 < this.side1) {
+				throw new Error("Треугольник с такими сторонами не существует");
+			}
+	
+		
 	}
 	get perimeter() {
 		return +(this.side1 + this.side2 + this.side3);
 	}
 	get area() {
-		let semiPer = (this.side1 + this.side2 + this.side3) * 0.5;
+		let semiPer = this.perimeter * 0.5;
 		let area = Math.sqrt(semiPer * (semiPer - this.side1) * (semiPer - this.side2) * (semiPer - this.side3));
 		return +(area.toFixed(3));
 	}
-	calculatorResult() {
-		if (side1 + side2 < side3 || side1 + side3 < side2 || side2 + side3 < side1) {
-			throw new Error("Треугольник с такими сторонами не существует");
-		}
-
-	}
+	
 }
 
 function getTriangle(side1, side2, side3) {
