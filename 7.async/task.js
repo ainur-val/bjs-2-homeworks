1d01ed8341c0a9514f693c1d8e1de985f3ff24e8
Aynur Valiev <ainur-val@mail.ru>
2025-03-21 22:27:58 +0500

    Седьмое задание полностью готово

---
+++ 7.async/task.js
@@ -0,0 +1,54 @@
+class AlarmClock {
+	constructor() {
+		this.alarmCollection = [];
+		this.intervalId = null;
+	}
+	addClock(time, callback) {
+		if (!time || !callback) {
+			throw new Error('Отсутствуют обязательные аргументы');
+		} else if (this.alarmCollection.includes(time) == true) {
+			console.warn('Уже присутствует звонок на это же время');
+		}
+		this.alarmCollection.push({
+			callback,
+			time,
+			canCall: true
+		});
+
+	}
+	removeClock(time) {
+		this.alarmCollection = this.alarmCollection.filter(del => del.time !== time);
+	}
+
+	getCurrentFormattedTime() {
+		return new Date().toLocaleTimeString().slice(0, -3);
+	}
+
+	start() {
+		if (this.intervalId) {
+		  return
+		}
+		this.intervalId = setInterval( () => {
+			this.alarmCollection.forEach(num => {
+				if (num.time === this.getCurrentFormattedTime() && num.canCall) {
+					num.canCall = false;
+					num.callback();
+				}
+			})
+		}, 1000)
+	}
+
+	stop() {
+		clearInterval(this.intervalId);
+		this.intervalId = null;
+	  }
+
+	resetAllCalls() {
+		this.alarmCollection.forEach(callCancel => callCancel.canCall = true);
+	}
+
+	clearAlarms() {
+		this.stop();
+		this.alarmCollection = [];
+	}
+}
